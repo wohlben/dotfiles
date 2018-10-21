d5f5a60ec8f5276fd02b2befcc7dda46480e20be
wohlben <bw@wohlben.de>
2018-10-21 22:03:13 +0200

    added simple zsh completion

--- zsh_custom/state.zsh
+++ zsh_custom/state.zsh
@@ -1,6 +1,18 @@
+
 function dps(){
+
 	local proj=${1:-novels}
 	cd ~/code/$proj
-	nohup pipenv run terminator -l scrapes &!
-	exit 
+	if [[ $proj == "novels" ]]; then
+		nohup pipenv run terminator -l scrapes &!
+		exit 
+	else
+		pipenv shell
+	fi
 }
+_dps(){
+	local dirs=( $(find ~/code/ -name Pipfile | grep -Po "(?<=code\/)(.*)(?=\/Pipfile)") )
+	_describe "projects " dirs
+}
+
+compdef _dps dps
